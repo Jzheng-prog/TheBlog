ef0e425f3108d44b22daf9166ccc5d46465b4853
Jzheng-prog <zhengjohn017@gmail.com>
2023-10-01 01:34:22 -0400

    basic server
    setup css

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    const fPath = path.join(__dirname, "index.html");
+    res.sendFile(fPath);
+});
+
+app.listen(3000);
